d461547cac5b4f7a2e9bfada41c122f3ad73decb
Rafael Cunha <rafael_pbc@yahoo.com.br>
2021-09-11 13:11:20 -0300

    Buscas agrupadas (GROUP BY)

--- backend/src/main/java/com/cunha/rafael/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/cunha/rafael/dsvendas/service/SaleService.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cunha.rafael.dsvendas.dto.SaleDTO;
+import com.cunha.rafael.dsvendas.dto.SaleSuccessDTO;
+import com.cunha.rafael.dsvendas.dto.SaleSumDTO;
 import com.cunha.rafael.dsvendas.entities.Sale;
 import com.cunha.rafael.dsvendas.repositories.SaleRepository;
 import com.cunha.rafael.dsvendas.repositories.SellerRepository;
@@ -28,6 +30,16 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 	}
 
 }
