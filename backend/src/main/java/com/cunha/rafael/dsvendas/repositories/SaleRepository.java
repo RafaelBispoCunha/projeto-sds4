e265fcae8cc9d4dec7330bcfa8485ff3af531e08
Rafael Cunha <rafael_pbc@yahoo.com.br>
2021-09-11 12:25:38 -0300

    Criada busca paginada por vendas

---
+++ backend/src/main/java/com/cunha/rafael/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.cunha.rafael.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cunha.rafael.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
---
+++ backend/src/main/java/com/cunha/rafael/dsvendas/service/SaleService.java
@@ -0,0 +1,33 @@
+package com.cunha.rafael.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cunha.rafael.dsvendas.dto.SaleDTO;
+import com.cunha.rafael.dsvendas.entities.Sale;
+import com.cunha.rafael.dsvendas.repositories.SaleRepository;
+import com.cunha.rafael.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+
+}
